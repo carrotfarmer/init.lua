60cdd4b1d8cb07a313aed59f2c4c480ef6bb7c31
Dhruva Srinivas <dhruvas17068@gmail.com>
2024-01-04 21:15:05 -0800

    ripped autoformat from kickstart

--- lua/carrotfarmer/remap.lua
+++ lua/carrotfarmer/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>p", vim.cmd.Ex)
 
 -- move blocks of stuff around
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -7,3 +7,9 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- search and replace
 vim.keymap.set("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- diagnostics
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
