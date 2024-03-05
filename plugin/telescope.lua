0fb4a50981352fe20d05cc4c7cadf95a4a4c7973
Dhruva Srinivas <dhruvas17068@gmail.com>
2024-03-05 12:46:37 -0800

    hijack netrw

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -1,7 +1,22 @@
 local builtin = require("telescope.builtin")
+local telescope = require("telescope")
+
+telescope.setup({
+    extensions = {
+        file_browser = {
+            initial_mode = "normal",
+            hijack_netrw = true
+        }
+    }
+})
+
+telescope.load_extension "file_browser"
 
 vim.keymap.set("n", "<leader>f", builtin.find_files, {})
 vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
+
+-- grep search
+vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 
 vim.keymap.set('n', '<leader>sf', function()
     builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
