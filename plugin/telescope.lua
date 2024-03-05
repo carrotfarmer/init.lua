d1ed872a692015a3e195f323321854fcec68224c
Dhruva Srinivas <dhruvas17068@gmail.com>
2024-03-05 12:41:22 -0800

    better fuzzy search

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -2,6 +2,10 @@ local builtin = require("telescope.builtin")
 
 vim.keymap.set("n", "<leader>f", builtin.find_files, {})
 vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
-vim.keymap.set("n", "<leader>s", function()
-				builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
+
+vim.keymap.set('n', '<leader>sf', function()
+    builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+        winblend = 0,
+        previewer = false,
+    })
+end, { desc = '[S]earch [F]uzzily?' });
