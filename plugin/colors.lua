5de425dcbdb44ded0456f44c0bd64de37e59ccb7
Dhruva Srinivas <dhruvas17068@gmail.com>
2026-06-03 09:01:47 +0530

    we back

--- plugin/colors.lua
+++ plugin/colors.lua
@@ -2,15 +2,16 @@ function ColorMyPencils(color)
     --vim.cmd.colorscheme("vim")
 
     -- Set the background and foreground colors of the popup menu
-    --vim.cmd([[highlight Pmenu guibg=#262626 guifg=#dcdcdc]])
+    -- vim.cmd([[highlight Pmenu guibg=#262626 guifg=#dcdcdc]])
     -- Set the background and foreground colors of the selected item in the popup menu
-    --vim.cmd([[highlight PmenuSel guibg=#87afd7 guifg=#000000]])
+    -- vim.cmd([[highlight PmenuSel guibg=#87afd7 guifg=#000000]])
     -- Set the color of the scrollbar in the popup menu
     --vim.cmd([[highlight PmenuSbar guibg=#3a3a3a guifg=#dcdcdc]])
 
-    -- vim.cmd.colorscheme("fleet")
+    --vim.cmd.colorscheme("fleet")
 
-    vim.cmd.colorscheme("gruber-darker")
+    -- vim.cmd.colorscheme("gruber-darker")
+    vim.cmd.colorscheme("rose-pine")
 end
 
 ColorMyPencils()
--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -1,91 +1,35 @@
-local lsp = require("lsp-zero")
+require("mason").setup()
 
-lsp.preset("recommended")
-
-require('lspconfig').jdtls.setup({})
-
--- mason setup
-require('mason').setup({})
-require('mason-lspconfig').setup({
-    ensure_installed = { "lua_ls", "tsserver", "rust_analyzer", "eslint", "jdtls" },
-    handlers = {
-        lsp.default_setup,
+require("mason-lspconfig").setup({
+    ensure_installed = {
+        "lua_ls",
+        "ts_ls",
+        "rust_analyzer",
+        "eslint",
     },
+    automatic_enable = true,
 })
 
--- [[ Configure nvim-cmp ]]
-local cmp = require 'cmp'
-local luasnip = require 'luasnip'
-require('luasnip.loaders.from_vscode').lazy_load()
-luasnip.config.setup {}
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-cmp.setup {
-    snippet = {
-        expand = function(args)
-            luasnip.lsp_expand(args.body)
-        end,
-    },
-    mapping = cmp.mapping.preset.insert {
-        ['<C-n>'] = cmp.mapping.select_next_item(),
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete {},
-        ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-        },
-        ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_locally_jumpable() then
-                luasnip.expand_or_jump()
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-        ['<C-j>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_locally_jumpable() then
-                luasnip.expand_or_jump()
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-        ['<C-k>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.locally_jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.locally_jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-    },
-    sources = {
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-    },
-}
+local on_attach = function(client, bufnr)
+    local opts = { buffer = bufnr }
 
-lsp.on_attach(function(client, bufnr)
-    local opts = { buffer = bufnr, remap = false }
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "<C-i>", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
+end
 
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "<C-i>", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>r", function() vim.lsp.buf.rename() end, opts)
-end)
-
-lsp.setup()
+for _, server in ipairs({
+    "lua_ls",
+    "ts_ls",
+    "rust_analyzer",
+    "eslint",
+}) do
+    vim.lsp.config(server, {
+        capabilities = capabilities,
+        on_attach = on_attach,
+    })
+end
