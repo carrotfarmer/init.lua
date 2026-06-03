b06da19d9db222674630ca94a49d2761d17fef77
Dhruva Srinivas <dhruvas17068@gmail.com>
2026-06-02 21:46:24 -0700

    make stuf work

--- lua/carrotfarmer/pluginboi.lua
+++ lua/carrotfarmer/pluginboi.lua
@@ -1,82 +1,105 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
+
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.5',
-        dependencies = { 'nvim-lua/plenary.nvim' }
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    config = function()
+      vim.keymap.set(
+        "n",
+        "<leader>b",
+        ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+        { noremap = true, desc = "Open File Explorer" }
+      )
+    end,
+  },
+  -- THEMES
+  { "rose-pine/neovim",     name = "rose-pine" },
+  "felipeagc/fleet-theme-nvim",
+  "blazkowolf/gruber-darker.nvim",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    build = ":TSUpdate",
+    lazy = false,
+    init = function()
+      local parsers = {
+        "c",
+        "cpp",
+        "lua",
+        "bash",
+        "go",
+        "html",
+        "java",
+        "javascript",
+        "typescript",
+        "json",
+        "python",
+        "rust",
+        "markdown",
+        "yaml",
+        "toml",
+      }
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          pcall(vim.treesitter.start)
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+      vim.schedule(function()
+        require("nvim-treesitter.install").install(parsers)
+      end)
+    end,
+  },
+  { "ThePrimeagen/harpoon" },
+  { "tpope/vim-fugitive" },
+  { "andweeb/presence.nvim" },
+  -- very important
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      open_mapping = [[<C-\>]],
     },
-    {
-        "nvim-telescope/telescope-file-browser.nvim",
-        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-        config = function()
-            vim.keymap.set(
-                "n",
-                "<leader>b",
-                ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
-                { noremap = true, desc = "Open File Explorer" }
-            )
-        end
-    },
-
-    -- THEMES
-    { 'rose-pine/neovim',                name = 'rose-pine' },
-    "felipeagc/fleet-theme-nvim",
-    "blazkowolf/gruber-darker.nvim",
-
-
-    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-    { "ThePrimeagen/harpoon" },
-    { "tpope/vim-fugitive" },
-    { "andweeb/presence.nvim" },
-
-    -- very important
-    {
-        "akinsho/toggleterm.nvim",
-        version = "*",
-        opts = {
-            open_mapping = [[<C-\>]]
-        }
-    },
-
-    { "mfussenegger/nvim-dap" },
-
-    -- lsp stuff
-    { 'williamboman/mason.nvim' },
-    { 'williamboman/mason-lspconfig.nvim' },
-    { 'VonHeikemen/lsp-zero.nvim',        branch = 'v3.x' },
-    { 'neovim/nvim-lspconfig' },
-    { 'hrsh7th/cmp-nvim-lsp' },
-    { 'hrsh7th/nvim-cmp' },
-    { 'L3MON4D3/LuaSnip' },
-    { "github/copilot.vim" },
-    { 'RaafatTurki/hex.nvim' },
-    { 'mfussenegger/nvim-jdtls' },
-
-    -- for commenting
-    { 'numToStr/Comment.nvim',            opts = {} },
-
-
-    { 'wakatime/vim-wakatime',            lazy = false },
-    { 'ThePrimeagen/vim-be-good' },
-
-
-    require("carrotfarmer.autofmt")
+  },
+  { "mfussenegger/nvim-dap" },
+  -- lsp stuff
+  { "williamboman/mason.nvim" },
+  { "williamboman/mason-lspconfig.nvim" },
+  { "VonHeikemen/lsp-zero.nvim",        branch = "v3.x" },
+  { "neovim/nvim-lspconfig" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/nvim-cmp" },
+  { "L3MON4D3/LuaSnip" },
+  { "github/copilot.vim" },
+  { "RaafatTurki/hex.nvim" },
+  { "mfussenegger/nvim-jdtls" },
+  -- for commenting
+  { "numToStr/Comment.nvim",            opts = {} },
+  { "wakatime/vim-wakatime",            lazy = false },
+  { "ThePrimeagen/vim-be-good" },
 })
+require("carrotfarmer.autofmt").setup()
 
 -- Comment.nvim remaps
 local comment_api = require("Comment.api")
-
-vim.keymap.set('n', '<leader>/', comment_api.toggle.linewise.current, { desc = '[/] Comment line' })
-vim.keymap.set('v', '<leader>/b', comment_api.toggle.blockwise.current, { desc = '[b] Comment block' })
+vim.keymap.set("n", "<leader>/", comment_api.toggle.linewise.current, { desc = "[/] Comment line" })
+vim.keymap.set("v", "<leader>/b", comment_api.toggle.blockwise.current, { desc = "[b] Comment block" })
--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -1,35 +1,57 @@
 require("mason").setup()
 
 require("mason-lspconfig").setup({
-    ensure_installed = {
-        "lua_ls",
-        "ts_ls",
-        "rust_analyzer",
-        "eslint",
-    },
-    automatic_enable = true,
+	ensure_installed = {
+		"lua_ls",
+		"ts_ls",
+		"rust_analyzer",
+		"eslint",
+		"clangd",
+	},
+	automatic_enable = true,
 })
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local on_attach = function(client, bufnr)
-    local opts = { buffer = bufnr }
+	local opts = { buffer = bufnr }
 
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "<C-i>", vim.lsp.buf.references, opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-    vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+	vim.keymap.set("n", "<C-i>", vim.lsp.buf.references, opts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+	vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
 end
 
 for _, server in ipairs({
-    "lua_ls",
-    "ts_ls",
-    "rust_analyzer",
-    "eslint",
+	"lua_ls",
+	"ts_ls",
+	"rust_analyzer",
+	"eslint",
+	"clangd",
 }) do
-    vim.lsp.config(server, {
-        capabilities = capabilities,
-        on_attach = on_attach,
-    })
+	vim.lsp.config(server, {
+		capabilities = capabilities,
+		on_attach = on_attach,
+	})
 end
+
+local cmp = require("cmp")
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<Tab>"] = cmp.mapping.select_next_item(),
+		["<S-Tab>"] = cmp.mapping.select_prev_item(),
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+	}),
+})
